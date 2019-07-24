d582a049c4bbf2f2176cc38f4db1442e2eeb995e
jonathanhovland <jonathan.hovland@yahoo.com>
2019-07-23 21:38:57 -0600

    setup eslint

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
 
 const PORT = process.env.PORT || 4000
 const app = express()
 
 // Middleware
 app.use(cors())
-app.use(morgan('dev'))
+app.use(morgan("dev"))
 
 // Router files
 
 // Routes
-app.get('/api/test', (req, res, next) => {
+// eslint-disable-next-line
+app.get("/api/test", (req, res, next) => {
     res.json({
-        message: 'Route working'
+        message: "Route working"
     })
 })
 
@@ -23,15 +24,16 @@ app.get('/api/test', (req, res, next) => {
 app.use(notFound)
 app.use(errorHandler)
 
+// eslint-disable-next-line
 function notFound(req, res, next) {
-  res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+    res.status(404).send({error: "Not found!", status: 404, url: req.originalUrl})
 }
 
 // eslint-disable-next-line
 function errorHandler(err, req, res, next) {
-  console.error('ERROR', err)
-  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
-  res.status(500).send({error: err.message, stack, url: req.originalUrl})
+    console.error("ERROR", err)
+    const stack =  process.env.NODE_ENV !== "production" ? err.stack : undefined
+    res.status(500).send({error: err.message, stack, url: req.originalUrl})
 }
 
 
